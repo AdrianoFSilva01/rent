ea0f7b71ae0ce213f8eda5f2923dbcdd114e6ca2
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-01-11 12:28:54 +0000

    Add HamburgerMenu component

---
+++ src/Components/HamburgerMenu/HamburgerMenu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hamburger-menu">
+        <hr />
+        <hr />
+        <hr />
+    </div>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+
+export default class HamburgerMenu extends Vue{
+
+}
+</script>
+
+<style scoped lang="postcss" src="./HamburgerMenu.pcss" />
