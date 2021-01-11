cfdfeb536aad914d87ff377d9bcf561dd44256db
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-01-11 13:45:44 +0000

    Add UnderlineTextAnimated component

---
+++ src/Components/UnderlineTextAnimated/UnderlineTextAnimated.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="underline-text-animated">
+        <p>
+            {{ text }}
+        </p>
+        <hr class="border-black" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+export default class UnderlineTextAnimated extends Vue{
+    @Prop() text: string | null = null;
+}
+</script>
+
+<style scoped lang="postcss" src="./UnderlineTextAnimated.pcss" />
