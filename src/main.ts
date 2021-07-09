3b9e610c8b43e6e65a3b019da4f7cb368353506d
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-07-09 12:58:10 +0100

    Remove config.wanHandler

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import mixins from "@/Extensions/Mixins/installMixins";
 import ApartmentsView from "@/Views/ApartmentsView/ApartmentsView.vue";
 import MainView from "@/Views/MainView/MainView.vue";
 import "reflect-metadata";
-import { App, ComponentPublicInstance, createApp } from "vue";
+import { App, createApp } from "vue";
 import { createRouter, createWebHistory, Router } from "vue-router";
 import MasterPage from "./MasterPage/MasterPage.vue";
 
@@ -16,19 +16,7 @@ const router: Router = createRouter({
         { path: "/", component: MainView},
         { path: "/apartments", component: ApartmentsView}
     ]
-}
-);
-
-app.config.warnHandler = (msg: string, instance: ComponentPublicInstance | null, trace: string): void => {
-    if (msg.includes('should not start with "$" or "_"')) {
-        return;
-    }
-    if (instance) {
-        console.error(`[Vue warn]: ${msg}\n${trace}\n\nProps:\n--->\t${JSON.stringify(instance.$props)}`);
-    } else {
-        console.error(`[Vue warn]: ${msg}\n${trace}`);
-    }
-};
+});
 
 app.use(mixins);
 app.use(directives);
