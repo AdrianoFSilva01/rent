3ee9eb950ecf99fb4d4677ccc6b26e1670ce13ca
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-07-16 18:30:29 +0100

    Fix not following when scrolling

--- src/Extensions/Directives/mouse-follower.ts
+++ src/Extensions/Directives/mouse-follower.ts
@@ -8,28 +8,43 @@ export default {
         let mouseFollowerX: number = el.offsetLeft;
         let mouseFollowerY: number = el.getBoundingClientRect().top + window.pageYOffset;
         let interval: number = 0;
+        let scrollY: number = 0;
+        let addScrollValue: number = 0;
+
+        const scroll = (): void => {
+            if(!scrollY) {
+                scrollY = window.scrollY;
+            } else if(scrollY !== window.scrollY) {
+                addScrollValue = window.scrollY - scrollY;
+            }
+        };
 
         el.addEventListener("mouseenter", (mouseEvent: MouseEvent) => {
             if(mouseFollower) {
+                window.addEventListener("scroll", scroll);
                 mouseFollowerX = mouseEvent.pageX;
                 mouseFollowerY = mouseEvent.pageY;
                 interval = setInterval(() => {
                     mouseFollowerX += (mouseX - mouseFollowerX) / 12;
-                    mouseFollowerY += (mouseY - mouseFollowerY) / 12;
+                    mouseFollowerY += ((mouseY + addScrollValue) - mouseFollowerY) / 12;
                     mouseFollower.style.left = `${mouseFollowerX - mouseFollower.offsetWidth / 2}px`;
                     mouseFollower.style.top = `${mouseFollowerY - mouseFollower.offsetHeight / 2}px`;
                     mouseFollower.style.display = "block";
+
                 }, 10);
             }
         });
 
         el.addEventListener("mousemove", (mouseEvent: MouseEvent) => {
+            addScrollValue = 0;
+            scrollY = 0;
             mouseX = mouseEvent.pageX;
             mouseY = mouseEvent.pageY;
         });
 
         el.addEventListener("mouseleave", () => {
             if (mouseFollower) {
+                window.removeEventListener("scroll", scroll);
                 clearInterval(interval);
                 mouseFollower.style.display = "none";
             }
