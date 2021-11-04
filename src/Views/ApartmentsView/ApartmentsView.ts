fcabb3207a9328628370a164db81ecaae4ffe1b4
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-04 12:40:11 +0000

    Clean code

--- src/Views/ApartmentsView/ApartmentsView.ts
+++ src/Views/ApartmentsView/ApartmentsView.ts
@@ -13,7 +13,7 @@ export default class ApartmentsView extends Vue {
 
     houses: Array<Array<string>> = [
         ["https://hipercentrodomovel.pt/wp-content/uploads/2019/06/QUCfoloBCFC003creta.jpg", "Deluxe Appartement", "max. 6 Persons", "approx. 70m²", "from 249€",
-            "Enough space to sleep, play, rest, sweat and cuddle: our deluxe apartments with two bedrooms, living kitchen and Swiss pine sauna on the loggia."],
+            "Enough space to sleep, play, rest, sweat and cuddle: our deluxe apartments with two bedrooms, living kitchen and sauna."],
         ["https://cf.bstatic.com/images/hotel/max1024x768/144/144383113.jpg", "Comfort Appartement", "max. 6 Persons", "approx. 60m²", "from 229€",
             "When one combines two bedrooms, a living room/kitchen and a loggia with sauna, all wishes are granted."],
         ["https://cf.bstatic.com/images/hotel/max1024x768/186/186088460.jpg", "Garden Appartement", "max. 6 Persons",  "approx. 100m²", "from 279€",
@@ -21,7 +21,7 @@ export default class ApartmentsView extends Vue {
         ["https://robbreport.com/wp-content/uploads/2019/02/hotel-ottilia-suite_bedroom.jpg?w=1000", "Comfort Studio", "4 Persons", "approx. 40m²", "from 139€",
             "A dream of a room: the comfort studio leaves a lasting impression in all our guests. And that’s not just because of the cosy sauna."],
         ["https://i.pinimg.com/originals/fe/19/aa/fe19aa5489cfb0a8ce4d5c6d2666c946.jpg", "Penthouse Studio", "max. 4 Persons", "approx. 40m²", "from 179€",
-            "For all those who want to go places – and want to stay there: our penthouse studio with loggia and Swiss pine sauna over the village roofs."],
+            "For all those who want to go places – and want to stay there: our penthouse studio with loggia and sauna over the village roofs."],
         ["https://cf.bstatic.com/images/hotel/max1024x768/153/153840518.jpg", "Garden Studio", "max. 4 Persons", "approx. 60m²", "from 179€",
             "For those who’d rather stay grounded: our exclusive garden studio on the ground floor. Private garden included."]];
     selectedIndex: number = 0;
@@ -34,9 +34,9 @@ export default class ApartmentsView extends Vue {
         }
 
         document.addEventListener("scroll", () => {
-            if(this.roomsList && this.roomDisplay)
-            {
-                if(pageYOffset >= this.roomsList.offsetTop && pageYOffset <= this.roomsList.offsetTop + this.roomsList.offsetHeight) {
+            if(this.roomsList && this.roomDisplay) {
+                const scrollBetweenRooms: boolean = pageYOffset >= this.roomsList.offsetTop && pageYOffset <= this.roomsList.offsetTop + this.roomsList.offsetHeight;
+                if(scrollBetweenRooms) {
                     this.scrollPosition = pageYOffset - this.roomsList.offsetTop;
                     if(this.scrollPosition < this.roomDisplay.offsetHeight) {
                         this.selectedIndex = 0;
