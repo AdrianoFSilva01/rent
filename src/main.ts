195a3aaba94b8dafb0c078888334a2c62c6a5e2c
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-04 12:35:45 +0000

    Add OurHouse View to route

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import directives from "@/Extensions/Directives/installDirectives";
 import mixins from "@/Extensions/Mixins/installMixins";
 import ApartmentsView from "@/Views/ApartmentsView/ApartmentsView.vue";
 import MainView from "@/Views/MainView/MainView.vue";
+import OurHouseView from "@/Views/OurHouseView/OurHouseView.vue";
 import "reflect-metadata";
 import { App, createApp } from "vue";
 import { createRouter, createWebHistory, Router } from "vue-router";
@@ -14,7 +15,8 @@ const router: Router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", component: MainView},
-        { path: "/apartments", component: ApartmentsView}
+        { path: "/apartments", component: ApartmentsView},
+        { path: "/ourhouse", component: OurHouseView}
     ]
 });
 
