df84e6298097d257b2a3af9f13fe4df8f75df03f
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-04 12:39:16 +0000

    Change names

--- src/Components/Dropdown/Dropdown.ts
+++ src/Components/Dropdown/Dropdown.ts
@@ -11,27 +11,14 @@ import { ModelSync, Prop, Ref, Watch } from "vue-property-decorator";
 })
 export default class Dropdown<T> extends Vue {
     @ModelSync("modelValue", "update:modelValue", { default: null} ) selected!: T | null;
+    @Ref() component!: HTMLElement | null;
     @Prop({default: null}) dropdownText!: string | null;
     @Prop({ default: null}) source!: Array<T> | null;
-    @Ref() component!: HTMLElement | null;
-
     @Prop() styleClass!: string;
 
-    bottomDistance: number = 0;
-
-    @Watch(nameof((x: Dropdown<T>) => x.selected))
+    @Watch(nameof((dropdown: Dropdown<T>) => dropdown.selected))
     onSelectedChange(): void {
         this.closeComponent();
-    }
-
-    mounted(): void {
-        if(this.component?.parentElement) {
-            this.bottomDistance = this.convertPixelsToRem(this.component?.parentElement?.offsetHeight);
-        }
-    }
-
-    convertPixelsToRem(pixels: number): number {
-        return pixels / parseFloat(getComputedStyle(document.documentElement).fontSize);
     }
 
     toggleComponent(): void {
@@ -49,8 +36,9 @@ export default class Dropdown<T> extends Vue {
     }
 
     setupPosition(distanceBottom: number, distanceTop: number): void {
-        if (this.component) {
-            if (this.$el.getBoundingClientRect().top + this.component.offsetHeight + distanceBottom + this.$el.offsetHeight > window.innerHeight) {
+        if(this.component) {
+            const openDownComponentFitPage: boolean = this.$el.getBoundingClientRect().top + this.component.offsetHeight + distanceBottom + this.$el.offsetHeight > window.innerHeight;
+            if (openDownComponentFitPage) {
                 this.component.classList.remove("top-8");
                 this.component.classList.add("bottom-9");
             } else {
