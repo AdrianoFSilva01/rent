d8e01a9f63b9f5c24472242ca861ada8155ea068
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-04 12:51:27 +0000

    Add fade-animation

--- src/Views/OurHouseView/OurHouseView.vue
+++ src/Views/OurHouseView/OurHouseView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="space-y-10">
-        <ImageSlideshow class="w-96 m-4" :images="images" />
-        <ImageReveal class="w-auto h-180 mx-20" :images="renovationImages" />
+        <ImageSlideshow class="w-96 m-4" :images="images" v-in-viewport="'fade-animation'" />
+        <ImageReveal class="w-auto h-180 mx-20" :images="renovationImages" v-in-viewport="'fade-animation'" />
     </div>
 </template>
 
