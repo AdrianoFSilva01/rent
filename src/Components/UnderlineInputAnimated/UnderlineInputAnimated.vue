5336248eea6d6fbb9e0cb4dec87ea1ba112c63df
Adriano Silva <AdrianoSilva16601@outlook.com>
2021-11-22 15:16:53 +0000

    Fix v-click-outside error

--- src/Components/UnderlineInputAnimated/UnderlineInputAnimated.vue
+++ src/Components/UnderlineInputAnimated/UnderlineInputAnimated.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-click-outside="underlineNone">
+    <div>
         <div class="input" v-bind="$attrs">
             <input class="w-full" :class="inputStyle" :placeholder="placeholderText" v-model="inputText" />
             <div id="underline" ref="underline" :class="underlineStyle" class="absolute w-0 border-b bottom-0 transition-all transform duration-main" />
